d2bf697a654146ffd806caa0768eb87775ac8b90
Donnie Flood <donnie@floodfx.com>
2022-02-09 10:12:19 -0700

    improve volunteers test coverage

--- src/examples/volunteers/data.test.ts
+++ src/examples/volunteers/data.test.ts
@@ -1,5 +1,5 @@
 
-import { Volunteer, VolunteerSchema } from './data';
+import { changeset, createVolunteer, getVolunteer, listVolunteers, updateVolunteer, Volunteer, VolunteerSchema } from './data';
 
 describe("test volunteers", () => {
   it("valid volunteer", () => {
@@ -69,5 +69,64 @@ describe("test volunteers", () => {
       expect(formatted.phone).not.toBeUndefined()
     }
   });
+
+  it("createVolunteer with valid volunteer updates in memory list", () => {
+    const createChangeset = createVolunteer({ name: "Jane Doe", phone: "123-456-7890" });
+    expect(createChangeset.valid).toBe(true);
+    expect(listVolunteers().length).toBe(1);
+    expect(listVolunteers()[0].name).toBe("Jane Doe");
+    expect(listVolunteers()[0].phone).toBe("123-456-7890");
+    const v = getVolunteer(createChangeset.data.id!);
+    expect(v!.name).toBe("Jane Doe");
+    expect(v!.phone).toBe("123-456-7890");
+  })
+
+  it("createVolunteer with invalid volunteer does not update in memeory list", () => {
+    const createChangeset = createVolunteer({ name: "J", phone: "123-456-7890" });
+    expect(createChangeset.valid).toBe(false);
+    expect(listVolunteers().length).toBe(0);
+  })
+
+  it("update with valid volunteer updates in memory list", () => {
+    const createChangeset = createVolunteer({ name: "Jane Doe", phone: "123-456-7890" });
+    expect(createChangeset.valid).toBe(true);
+    expect(listVolunteers().length).toBe(1);
+
+    const v = getVolunteer(createChangeset.data.id!);
+    expect(v!.checked_out).toBe(false);
+
+    const updateChangeset = updateVolunteer(v!, { checked_out: true });
+    expect(updateChangeset.valid).toBe(true);
+
+    const v2 = getVolunteer(createChangeset.data.id!);
+    expect(v2!.checked_out).toBe(true);
+  })
+
+  it("updateVolunteer with invalid volunteer does not update in memeory list", () => {
+    const createChangeset = createVolunteer({ name: "Jane Doe", phone: "123-456-7890" });
+    expect(createChangeset.valid).toBe(true);
+    expect(listVolunteers().length).toBe(1);
+
+    const v = getVolunteer(createChangeset.data.id!);
+    expect(v!.name).toBe("Jane Doe");
+
+    const updateChangeset = updateVolunteer(v!, { name: "J" });
+    expect(updateChangeset.valid).toBe(false);
+
+    const v2 = getVolunteer(createChangeset.data.id!);
+    expect(v2!.name).toBe("Jane Doe");
+  })
+
+  it("valid changeset returns undefined errors", () => {
+    const cs = changeset({}, { name: "Jane Doe", phone: "123-456-7890" });
+    expect(cs.valid).toBe(true);
+    expect(cs.errors).toBeUndefined();
+  })
+
+  it("invalid changeset returns errors", () => {
+    const cs = changeset({}, { name: "", phone: "123-456-7890" });
+    expect(cs.valid).toBe(false);
+    expect(cs.errors!.name).not.toBeUndefined();
+  })
 
 });
