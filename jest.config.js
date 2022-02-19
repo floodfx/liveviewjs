0383063a4c9053c002eae553e3534eed9962406b
Donnie Flood <donnie@floodfx.com>
2022-02-19 09:05:48 -0700

    remove lcov html reports; only save clover.xml and coverage-final.json; fixes #40

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
   coverageDirectory: 'coverage',
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
+  coverageReporters: ['json', 'clover'],
   roots: ['<rootDir>/src'],
   verbose: true,
   coverageThreshold: {
