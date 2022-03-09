b0147a6992ce070071512fac5d30befe64426ac7
Donnie Flood <donnie@floodfx.com>
2022-03-09 13:20:07 -0700

    updated based on  renaming and added async support to

--- src/server/component/base_component.ts
+++ src/server/component/base_component.ts
@@ -1,10 +1,10 @@
 import { SessionData } from "express-session";
-import { LiveViewComponent, LiveViewMetadata, LiveViewMountParams, LiveViewSocket, LiveViewTemplate, StringPropertyValues } from ".";
+import { LiveView, LiveViewMeta, LiveViewMountParams, LiveViewSocket, LiveViewTemplate, StringPropertyValues } from ".";
 
-export abstract class BaseLiveViewComponent<T, P> implements LiveViewComponent<T, P> {
+export abstract class BaseLiveViewComponent<T, P> implements LiveView<T, P> {
 
   abstract mount(params: LiveViewMountParams, session: Partial<SessionData>, socket: LiveViewSocket<T>): T | Promise<T>;
-  abstract render(context: T, meta: LiveViewMetadata): LiveViewTemplate | Promise<LiveViewTemplate>;
+  abstract render(context: T, meta: LiveViewMeta): LiveViewTemplate | Promise<LiveViewTemplate>;
 
   handleParams(params: StringPropertyValues<P>, url: string, socket: LiveViewSocket<T>): T | Promise<T> {
     return socket.context;
