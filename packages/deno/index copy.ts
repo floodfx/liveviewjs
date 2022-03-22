3c92b06cd8b5f3d1eab6c380d2a124efed0eb111
Donnie Flood <donnie@floodfx.com>
2022-03-22 10:34:38 -0600

    remove deno working directory

--- packages/deno/index copy.ts
+++
@@ -1,9 +0,0 @@
-import { serve, Application } from "./deps.ts"
-
-const app = new Application();
-
-app.use((ctx) => {
-  ctx.response.body = "Hello dsaffads!";
-});
-
-await app.listen({ port: 8000 });
--- packages/deno/session_data.ts
+++
@@ -1,7 +0,0 @@
-import type { SessionDataProvider } from "../server/common/session_data_provider.ts";
-
-// export class DenoSessionDataProvder<SessionData> implements SessionDataProvider<SessionData, Request> {
-//   sessionData(req: Request): Promise<SessionData> {
-//     return Promise.resolve({ csrfToken: "csrfToken" });
-//   }
-// }
