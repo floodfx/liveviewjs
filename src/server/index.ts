e3e8f9111029ac1d831d6a5e4ac1a8f28164361d
Donnie Flood <donnie@floodfx.com>
2022-05-05 22:38:22 -0600

    try adding singleProcessPubSub directly

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,7 @@ export * from "./adaptor";
 export * from "./changeset";
 export * from "./live";
 export * from "./pubsub";
+export * from "./pubsub/singleProcessPubSub";
 export * from "./session";
 export * from "./socket";
 export * from "./templates";
