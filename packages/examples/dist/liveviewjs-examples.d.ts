0371f72542915d8fd5e6b07d838f7d3bb7e811f7
Donnie Flood <donnie@floodfx.com>
2022-05-05 22:01:00 -0600

    rebuild

--- packages/examples/dist/liveviewjs-examples.d.ts
+++ packages/examples/dist/liveviewjs-examples.d.ts
@@ -13,14 +13,14 @@ interface Store {
     hours: string;
 }
 
-interface Context$9 {
+interface Context$a {
     zip: string;
     city: string;
     stores: Store[];
     matches: string[];
     loading: boolean;
 }
-declare type Events$9 = {
+declare type Events$a = {
     type: "zip-search";
     zip: string;
 } | {
@@ -37,14 +37,14 @@ declare type Infos$1 = {
     type: "run_city_search";
     city: string;
 };
-declare class AutocompleteLiveView extends BaseLiveView<Context$9, Events$9, Infos$1> {
-    mount(params: LiveViewMountParams, session: Partial<SessionData>, socket: LiveViewSocket<Context$9>): void;
+declare class AutocompleteLiveView extends BaseLiveView<Context$a, Events$a, Infos$1> {
+    mount(params: LiveViewMountParams, session: Partial<SessionData>, socket: LiveViewSocket<Context$a>): void;
+    handleEvent(event: Events$a, socket: LiveViewSocket<Context$a>): void;
+    handleInfo(info: Infos$1, socket: LiveViewSocket<Context$a>): void;
+    render(context: Context$a): liveviewjs.HtmlSafeString;
     renderStoreStatus(store: Store): liveviewjs.HtmlSafeString;
     renderStore(store: Store): liveviewjs.HtmlSafeString;
     renderLoading(): liveviewjs.HtmlSafeString;
-    render(context: Context$9): liveviewjs.HtmlSafeString;
-    handleEvent(event: Events$9, socket: LiveViewSocket<Context$9>): void;
-    handleInfo(info: Infos$1, socket: LiveViewSocket<Context$9>): void;
 }
 
 declare class DecarbonizeLiveView extends BaseLiveView {
@@ -56,26 +56,26 @@ declare type VehicleType = "gas" | "electric" | "hybrid" | "dontHave";
 declare type SpaceHeatingType = "gas" | "oil" | "electric" | "radiant" | "heatpump" | "other" | "notSure";
 declare type GridElectricityType = "grid" | "renewable" | "commSolar" | "notSure";
 
-interface Context$8 {
+interface Context$9 {
     vehicle1: VehicleType;
     vehicle2: VehicleType;
     spaceHeating: SpaceHeatingType;
     gridElectricity: GridElectricityType;
     carbonFootprintTons: number;
 }
-declare type Events$8 = {
+declare type Events$9 = {
     type: "calculate";
     vehicle1: VehicleType;
     vehicle2: VehicleType;
     spaceHeating: SpaceHeatingType;
     gridElectricity: GridElectricityType;
 };
-declare class CalculatorLiveComponent extends BaseLiveComponent<Context$8, Events$8> {
-    render(context: Context$8, meta: LiveComponentMeta): LiveViewTemplate;
-    renderFootprint(carbonFootprintTons: number, myself: number, context: Context$8): liveviewjs.HtmlSafeString;
-    renderChart(id: string, context: Context$8): liveviewjs.HtmlSafeString;
-    handleEvent(event: Events$8, socket: LiveComponentSocket<Context$8>): void;
-    getChartData(id: string, context: Context$8): {
+declare class CalculatorLiveComponent extends BaseLiveComponent<Context$9, Events$9> {
+    render(context: Context$9, meta: LiveComponentMeta): LiveViewTemplate;
+    renderFootprint(carbonFootprintTons: number, myself: number, context: Context$9): liveviewjs.HtmlSafeString;
+    renderChart(id: string, context: Context$9): liveviewjs.HtmlSafeString;
+    handleEvent(event: Events$9, socket: LiveComponentSocket<Context$9>): void;
+    getChartData(id: string, context: Context$9): {
         chartId: string;
         data: {
             labels: string[];
@@ -87,24 +87,26 @@ declare class CalculatorLiveComponent extends BaseLiveComponent<Context$8, Event
     };
 }
 
-interface Context$7 {
-    seats: number;
-    amount: number;
+declare type PhotoSize = "4x6" | "5x7" | "8x10" | "10x13" | "11x14";
+interface Context$8 {
+    photoSizeIndex: number;
+    photoSize: PhotoSize;
+    cost: number;
 }
-declare type Events$7 = {
+declare type Events$8 = {
     type: "update";
-    seats: string;
+    photoSizeIndex: string;
 };
-declare class LicenseLiveView extends BaseLiveView<Context$7, Events$7> {
-    mount(params: LiveViewMountParams, session: Partial<SessionData>, socket: LiveViewSocket<Context$7>): void;
-    render(context: Context$7): liveviewjs.HtmlSafeString;
-    handleEvent(event: Events$7, socket: LiveViewSocket<Context$7>): void;
+declare class PrintsLiveView extends BaseLiveView<Context$8, Events$8> {
+    mount(params: LiveViewMountParams, session: Partial<SessionData>, socket: LiveViewSocket<Context$8>): void;
+    render(context: Context$8, meta: LiveViewMeta): liveviewjs.HtmlSafeString;
+    handleEvent(event: Events$8, socket: LiveViewSocket<Context$8>): void;
 }
 
-interface Context$6 {
-    brightness: number;
+interface Context$7 {
+    volume: number;
 }
-declare type Events$6 = {
+declare type Events$7 = {
     type: "on";
 } | {
     type: "off";
@@ -116,18 +118,18 @@ declare type Events$6 = {
     type: "key_update";
     key: string;
 };
-declare class LightLiveView extends BaseLiveView<Context$6, Events$6> {
-    mount(params: LiveViewMountParams, session: Partial<SessionData>, socket: LiveViewSocket<Context$6>): void;
-    render(context: Context$6): liveviewjs.HtmlSafeString;
-    handleEvent(event: Events$6, socket: LiveViewSocket<Context$6>): void;
+declare class VolumeLiveView extends BaseLiveView<Context$7, Events$7> {
+    mount(params: LiveViewMountParams, session: Partial<SessionData>, socket: LiveViewSocket<Context$7>): void;
+    render(context: Context$7, meta: LiveViewMeta): liveviewjs.HtmlSafeString;
+    handleEvent(event: Events$7, socket: LiveViewSocket<Context$7>): Promise<void>;
 }
 
-interface Context$5 {
+interface Context$6 {
     zip: string;
     stores: Store[];
     loading: boolean;
 }
-declare type Events$5 = {
+declare type Events$6 = {
     type: "zip-search";
     zip: string;
 };
@@ -135,14 +137,14 @@ declare type Infos = {
     type: "run_zip_search";
     zip: string;
 };
-declare class SearchLiveView extends BaseLiveView<Context$5, Events$5, Infos> {
-    mount(params: LiveViewMountParams, session: Partial<SessionData>, socket: LiveViewSocket<Context$5>): void;
+declare class SearchLiveView extends BaseLiveView<Context$6, Events$6, Infos> {
+    mount(params: LiveViewMountParams, session: Partial<SessionData>, socket: LiveViewSocket<Context$6>): void;
     renderStoreStatus(store: Store): liveviewjs.HtmlSafeString;
     renderStore(store: Store): liveviewjs.HtmlSafeString;
     renderLoading(): liveviewjs.HtmlSafeString;
-    render(context: Context$5): liveviewjs.HtmlSafeString;
-    handleEvent(event: Events$5, socket: LiveViewSocket<Context$5>): void;
-    handleInfo(info: Infos, socket: LiveViewSocket<Context$5>): void;
+    render(context: Context$6, meta: LiveViewMeta): liveviewjs.HtmlSafeString;
+    handleEvent(event: Events$6, socket: LiveViewSocket<Context$6>): void;
+    handleInfo(info: Infos, socket: LiveViewSocket<Context$6>): void;
 }
 
 interface Donation$1 {
@@ -157,41 +159,41 @@ interface Options {
     page: number;
     perPage: number;
 }
-interface Context$4 {
+interface Context$5 {
     options: Options;
     donations: Donation$1[];
 }
-declare type Events$4 = {
+declare type Events$5 = {
     type: "select-per-page";
     perPage: string;
 };
-declare class PaginateLiveView extends BaseLiveView<Context$4, Events$4> {
-    mount(params: LiveViewMountParams, session: Partial<SessionData>, socket: LiveViewSocket<Context$4>): void;
-    handleParams(url: URL, socket: LiveViewSocket<Context$4>): void;
-    render(context: Context$4): HtmlSafeString;
-    handleEvent(event: Events$4, socket: LiveViewSocket<Context$4>): void;
+declare class PaginateLiveView extends BaseLiveView<Context$5, Events$5> {
+    mount(params: LiveViewMountParams, session: Partial<SessionData>, socket: LiveViewSocket<Context$5>): void;
+    handleParams(url: URL, socket: LiveViewSocket<Context$5>): void;
+    render(context: Context$5): HtmlSafeString;
+    handleEvent(event: Events$5, socket: LiveViewSocket<Context$5>): void;
     pageLinks(page: number, perPage: number): HtmlSafeString;
     paginationLink(text: string, pageNum: number, perPageNum: number, className: string): HtmlSafeString;
     renderDonations(donations: Donation$1[]): HtmlSafeString[];
     expiresDecoration(donation: Donation$1): number | HtmlSafeString;
 }
 
-interface Context$3 {
+interface Context$4 {
     newOrders: number;
     salesAmount: number;
-    satisfaction: number;
+    rating: number;
 }
-declare type Events$3 = {
+declare type Events$4 = {
     type: "refresh";
 };
 declare type Info = {
     type: "tick";
 };
-declare class SalesDashboardLiveView extends BaseLiveView<Context$3, Events$3, Info> {
-    mount(params: LiveViewMountParams, session: Partial<SessionData>, socket: LiveViewSocket<Context$3>): void;
-    render(context: Context$3): liveviewjs.HtmlSafeString;
-    handleEvent(event: Events$3, socket: LiveViewSocket<Context$3>): void;
-    handleInfo(info: Info, socket: LiveViewSocket<Context$3>): void;
+declare class DashboardLiveView extends BaseLiveView<Context$4, Events$4, Info> {
+    mount(params: LiveViewMountParams, session: Partial<SessionData>, socket: LiveViewSocket<Context$4>): void;
+    render(context: Context$4): liveviewjs.HtmlSafeString;
+    handleEvent(event: Events$4, socket: LiveViewSocket<Context$4>): void;
+    handleInfo(info: Info, socket: LiveViewSocket<Context$4>): void;
 }
 
 interface Server {
@@ -206,18 +208,18 @@ interface Server {
     last_commit_message: string;
 }
 
-interface Context$2 {
+interface Context$3 {
     servers: Server[];
     selectedServer: Server;
 }
-declare type Events$2 = {
+declare type Events$3 = {
     type: "select";
     id: string;
 };
-declare class ServersLiveView extends BaseLiveView<Context$2, Events$2> {
-    mount(params: LiveViewMountParams, session: Partial<SessionData>, socket: LiveViewSocket<Context$2>): void;
-    handleParams(url: URL, socket: LiveViewSocket<Context$2>): void;
-    render(context: Context$2): LiveViewTemplate;
+declare class ServersLiveView extends BaseLiveView<Context$3, Events$3> {
+    mount(params: LiveViewMountParams, session: Partial<SessionData>, socket: LiveViewSocket<Context$3>): void;
+    handleParams(url: URL, socket: LiveViewSocket<Context$3>): void;
+    render(context: Context$3): LiveViewTemplate;
     private link_body;
 }
 
@@ -237,11 +239,11 @@ interface SortOptions {
     sortby: keyof Donation;
     sortOrder: "asc" | "desc";
 }
-interface Context$1 {
+interface Context$2 {
     options: PaginateOptions & SortOptions;
     donations: Donation[];
 }
-declare type Events$1 = {
+declare type Events$2 = {
     type: "select-per-page";
     perPage: string;
 } | {
@@ -249,11 +251,11 @@ declare type Events$1 = {
     sortby: string;
     sortOrder: string;
 };
-declare class SortLiveView extends BaseLiveView<Context$1, Events$1> {
-    mount(params: LiveViewMountParams, session: Partial<SessionData>, socket: LiveViewSocket<Context$1>): void;
-    handleParams(url: URL, socket: LiveViewSocket<Context$1>): void;
-    render(context: Context$1): HtmlSafeString;
-    handleEvent(event: Events$1, socket: LiveViewSocket<Context$1>): void;
+declare class SortLiveView extends BaseLiveView<Context$2, Events$2> {
+    mount(params: LiveViewMountParams, session: Partial<SessionData>, socket: LiveViewSocket<Context$2>): void;
+    handleParams(url: URL, socket: LiveViewSocket<Context$2>): void;
+    render(context: Context$2, meta: LiveViewMeta): HtmlSafeString;
+    handleEvent(event: Events$2, socket: LiveViewSocket<Context$2>): void;
     sort_emoji(sortby: keyof Donation, sortby_value: string, sortOrder: "asc" | "desc"): "" | "👇" | "☝️";
     pageLinks(page: number, perPage: number, sortby: keyof Donation, sortOrder: "asc" | "desc"): HtmlSafeString;
     paginationLink(text: string, pageNum: number, perPageNum: number, sortby: keyof Donation, sortOrder: "asc" | "desc", className: string): HtmlSafeString;
@@ -271,11 +273,11 @@ declare type VolunteerMutationEvent = {
     volunteer: Volunteer;
 };
 
-interface Context {
+interface Context$1 {
     volunteers: Volunteer[];
     changeset: LiveViewChangeset<Volunteer>;
 }
-declare type Events = {
+declare type Events$1 = {
     type: "save";
     name: string;
     phone: string;
@@ -287,12 +289,34 @@ declare type Events = {
     type: "toggle-status";
     id: string;
 };
-declare class VolunteersLiveView extends BaseLiveView<Context, Events, VolunteerMutationEvent> {
-    mount(params: LiveViewMountParams, session: Partial<SessionData>, socket: LiveViewSocket<Context>): void;
-    render(context: Context, meta: LiveViewMeta): liveviewjs.HtmlSafeString;
+declare class VolunteersLiveView extends BaseLiveView<Context$1, Events$1, VolunteerMutationEvent> {
+    mount(params: LiveViewMountParams, session: Partial<SessionData>, socket: LiveViewSocket<Context$1>): void;
+    render(context: Context$1, meta: LiveViewMeta): liveviewjs.HtmlSafeString;
     renderVolunteer(volunteer: Volunteer): liveviewjs.HtmlSafeString;
-    handleEvent(event: Events, socket: LiveViewSocket<Context>): void;
-    handleInfo(event: VolunteerMutationEvent, socket: LiveViewSocket<Context>): void;
+    handleEvent(event: Events$1, socket: LiveViewSocket<Context$1>): void;
+    handleInfo(event: VolunteerMutationEvent, socket: LiveViewSocket<Context$1>): void;
 }
 
-export { AutocompleteLiveView, CalculatorLiveComponent, Context$6 as Context, DecarbonizeLiveView, Events$6 as Events, LicenseLiveView, LightLiveView, PaginateLiveView, PaginateOptions, SalesDashboardLiveView, SearchLiveView, ServersLiveView, SortLiveView, SortOptions, VolunteersLiveView };
+interface Context {
+    count: number;
+}
+declare type Events = {
+    type: "increment";
+} | {
+    type: "decrement";
+};
+declare class CounterLiveView extends BaseLiveView<Context, Events> {
+    mount(params: LiveViewMountParams, session: Partial<SessionData>, socket: LiveViewSocket<Context>): void;
+    handleEvent(event: Events, socket: LiveViewSocket<Context>): void;
+    render(context: Context, meta: LiveViewMeta): liveviewjs.HtmlSafeString;
+}
+
+interface RouteDetails {
+    label: string;
+    path: string;
+    summary: string;
+    tags: string[];
+}
+declare const routeDetails: RouteDetails[];
+
+export { AutocompleteLiveView, CalculatorLiveComponent, Context$7 as Context, CounterLiveView, DashboardLiveView, DecarbonizeLiveView, Events$7 as Events, PaginateLiveView, PaginateOptions, PrintsLiveView, RouteDetails, SearchLiveView, ServersLiveView, SortLiveView, SortOptions, VolumeLiveView, VolunteersLiveView, routeDetails };
