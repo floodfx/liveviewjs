4acf24039a6e8677a842935e6fdb940ea9a6ab83
Donnie Flood <donnie@floodfx.com>
2022-06-27 21:56:19 -0600

    update README for deno deploy cmd and handle write failure on deno deploy

--- packages/deno/src/deno/jwtSerDe.ts
+++ packages/deno/src/deno/jwtSerDe.ts
@@ -4,18 +4,23 @@ import { create, SerDe, SessionData, verify } from "../deps.ts";
 // result in session data passed from http to ws that will fail deserialization
 const algo: HmacKeyGenParams = { name: "HMAC", hash: "SHA-512" };
 const keyUsages: KeyUsage[] = ["sign", "verify"];
+let jsonWebKey: JsonWebKey;
 let key: CryptoKey;
 try {
   try {
     // try to load key from local file
     const data = Deno.readFileSync("./key.json");
-    const jsonWebKey: JsonWebKey = JSON.parse(new TextDecoder().decode(data));
+    jsonWebKey = JSON.parse(new TextDecoder().decode(data));
     key = await crypto.subtle.importKey("jwk", jsonWebKey, algo, true, keyUsages);
   } catch (_e) {
     // if key.json doesn't exist, generate a new key and save it to key.json
     key = await crypto.subtle.generateKey(algo, true, keyUsages);
-    const jsonWebKey = await crypto.subtle.exportKey("jwk", key);
-    Deno.writeFileSync("./key.json", new TextEncoder().encode(JSON.stringify(jsonWebKey)));
+    jsonWebKey = await crypto.subtle.exportKey("jwk", key);
+    try {
+      Deno.writeFileSync("./key.json", new TextEncoder().encode(JSON.stringify(jsonWebKey)));
+    } catch (_e) {
+      console.error("Could not save key.json to disk");
+    }
   }
 } catch (e) {
   console.error(e);
