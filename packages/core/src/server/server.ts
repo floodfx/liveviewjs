b9ea1ad1ff24695bae113931bcb0cc2676cebbe4
Donnie Flood <donnie@floodfx.com>
2022-09-22 18:14:09 -0600

    Change LiveViewAdaptor API

--- packages/core/src/server/server.ts
+++ packages/core/src/server/server.ts
@@ -1,10 +1,8 @@
-import { WsMessageRouter } from "./socket";
-
 /**
  * Interface for LiveViewServerAdaptors to implement for a given runtime and web server.
  * e.g. NodeExpressServerAdaptor or DenoOakServerAdaptor
  */
-export interface LiveViewServerAdaptor<TMiddlewareInterface> {
-  httpMiddleware(): TMiddlewareInterface;
-  wsRouter(): WsMessageRouter;
+export interface LiveViewServerAdaptor<THttpMiddleware, TWsMiddleware> {
+  httpMiddleware(): THttpMiddleware;
+  wsMiddleware(): TWsMiddleware;
 }
