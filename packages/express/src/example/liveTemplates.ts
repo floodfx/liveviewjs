cf14bd68a7ddcf6d8da65d81606edb4ba918ba44
Donnie Flood <donnie@floodfx.com>
2022-09-25 09:41:41 -0500

    - add favicons to deno and express - update deno run script in readme - clean up express tsconfig

--- packages/express/src/example/liveTemplates.ts
+++ packages/express/src/example/liveTemplates.ts
@@ -31,12 +31,6 @@ export const htmlPageTemplate: LiveViewHtmlPageTemplate = (
     <!DOCTYPE html>
     <html lang="en">
       <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0cafba" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="theme-color" content="#ffffff" />
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
