d6ecfe3b4d993bb8920352ca89a6fd941971780b
Donnie Flood <donnie@floodfx.com>
2026-07-30 20:49:20 -0600

    feat(infra): add liveviewjs top-level meta-package re-exporting core, web, and router

---
+++ packages/liveviewjs/src/index.ts
@@ -0,0 +1,4 @@
+// Top-level meta-package re-exporting @liveviewjs/core, @liveviewjs/web, and @liveviewjs/router
+export * from "@liveviewjs/core";
+export * from "@liveviewjs/web";
+export * from "@liveviewjs/router";
